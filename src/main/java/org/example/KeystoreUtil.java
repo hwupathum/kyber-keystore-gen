1fb1bb322351c5ec7094686b578124f1c5eba69c
Udara Pathum <46132469+hwupathum@users.noreply.github.com>
2024-05-16 14:25:15 +0530

    Add keystore methods

---
+++ src/main/java/org/example/KeystoreUtil.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.Date;
+
+public class KeystoreUtil {
+
+    private static final String SIGNING_ALGORITHM = "SHA256withRSA";
+    private static final String ISSUER = "localhost, OU=WSO2, O=WSO2, L=Mountain View, ST=CA, C=US";
+
+
+    private KeystoreUtil() {}
+
+    public static void addSelfSignedCertificate(KeyStore keyStore, String alias, KeyPair keyPair, String password)
+            throws CertificateException, OperatorCreationException, KeyStoreException {
+
+        X509Certificate
+                caCertificate = KeystoreUtil.generateCertificate(keyPair.getPublic(), keyPair.getPrivate(), ISSUER, ISSUER);
+        keyStore.setKeyEntry(alias, keyPair.getPrivate(), password.toCharArray(), new Certificate[]{caCertificate});
+    }
+
+    public static void addCertificate(KeyStore keyStore, String alias, KeyPair signingKeyPair, KeyPair keyPair, String password)
+            throws CertificateException, OperatorCreationException, KeyStoreException {
+
+        X509Certificate signingCert = KeystoreUtil.generateCertificate(signingKeyPair.getPublic(), signingKeyPair.getPrivate(), ISSUER, ISSUER);
+        PrivateKey signingKey = signingKeyPair.getPrivate();
+        X509Certificate certificate = KeystoreUtil.generateCertificate(keyPair.getPublic(), signingKey, ISSUER, ISSUER);
+        Certificate[] chain = new Certificate[]{certificate, signingCert};
+        keyStore.setKeyEntry(alias, keyPair.getPrivate(), password.toCharArray(), chain);
+    }
+
+    public static KeyPair generateKeyPair(String algorithm, AlgorithmParameterSpec algorithmParameterSpec, String provider)
+            throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(algorithm, provider);
+        keyPairGenerator.initialize(algorithmParameterSpec);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+
+    public static X509Certificate generateCertificate(PublicKey certPublicKey, PrivateKey signingPrivateKey, String issuerName, String subjectName)
+            throws OperatorCreationException, CertificateException {
+
+        // Generate a signed X.509 certificate
+        X500Name issuer = new X500Name("CN=" + issuerName);
+        X500Name subject = new X500Name("CN=" + subjectName);
+        Date notBefore = new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24 * 30);
+        Date notAfter = new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365 * 10));
+        SubjectPublicKeyInfo subPubKeyInfo = SubjectPublicKeyInfo.getInstance(certPublicKey.getEncoded());
+        X509v3CertificateBuilder certBuilder = new X509v3CertificateBuilder(
+                issuer,
+                BigInteger.valueOf(new SecureRandom().nextInt()),
+                notBefore,
+                notAfter,
+                subject,
+                subPubKeyInfo
+        );
+        // Use certificate converter to create the X.509 certificate
+        JcaX509CertificateConverter certConverter = new JcaX509CertificateConverter()
+                .setProvider(BouncyCastleProvider.PROVIDER_NAME);
+        return certConverter.getCertificate(certBuilder.build(
+                new org.bouncycastle.operator.jcajce.JcaContentSignerBuilder(SIGNING_ALGORITHM)
+                        .setProvider(BouncyCastleProvider.PROVIDER_NAME).build(signingPrivateKey)));
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,61 @@
 package org.example;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
+import org.bouncycastle.pqc.jcajce.spec.KyberParameterSpec;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.spec.RSAKeyGenParameterSpec;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String KEYSTORE_PASSWORD = "ballerina";
+    private static final String KEYSTORE_TYPE = "PKCS12";
+    private static final String KEY_ALIAS = "mlkem-keypair";
+    private static final String KEYSTORE_NAME = "mlkem-keystore.p12";
 
-        System.out.println("Hello world!");
+    private static void createKeyStore(KeyStore keyStore)
+            throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException,
+            CertificateException, KeyStoreException, OperatorCreationException {
+
+        // Generating a key pair for KEM
+        KeyPair keyPair = KeystoreUtil.generateKeyPair("Kyber768", KyberParameterSpec.kyber768,
+                BouncyCastlePQCProvider.PROVIDER_NAME);
+        // Create a KeyPairGenerator for the signing key pair
+        KeyPair signingKeyPair = KeystoreUtil.generateKeyPair("RSA",
+                new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4), BouncyCastleProvider.PROVIDER_NAME);
+
+        // Create a Java Keystore and add the generated key pair and certificate
+        KeystoreUtil.addSelfSignedCertificate(keyStore, "issuer", signingKeyPair, KEYSTORE_PASSWORD);
+        KeystoreUtil.addCertificate(keyStore, KEY_ALIAS, signingKeyPair, keyPair, KEYSTORE_PASSWORD);
+        System.out.println("Key pair and certificate added to Keystore successfully!");
+
+    }
+
+    public static void main(String[] args)
+            throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, NoSuchProviderException, OperatorCreationException {
+
+        KeyStore keyStore;
+
+        Security.addProvider(new BouncyCastlePQCProvider());
+        Security.addProvider(new BouncyCastleProvider());
+
+        keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
+        keyStore.load(null, KEYSTORE_PASSWORD.toCharArray());
+        createKeyStore(keyStore);
+
+        // Store keystore in a file
+        keyStore.store(new FileOutputStream(KEYSTORE_NAME), KEYSTORE_PASSWORD.toCharArray());
     }
 }
